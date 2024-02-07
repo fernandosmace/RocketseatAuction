bb16401caf60744da5c7cd37ff3e896087648dbe
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2024-02-07 20:26:58 -0300

    Add Enum for the condition of an Item.

--- src/RocketseatAuction.Domain/Entities/Item.cs
+++ src/RocketseatAuction.Domain/Entities/Item.cs
@@ -1,9 +1,11 @@
-﻿namespace RocketseatAuction.Domain.Entities;
+﻿using RocketseatAuction.Domain.Enums;
+
+namespace RocketseatAuction.Domain.Entities;
 public class Item : Entity
 {
     public string Name { get; set; } = string.Empty;
     public string Brand { get; set; } = string.Empty;
-    public int Condition { get; set; }
+    public ECondition Condition { get; set; }
     public decimal BasePrice { get; set; }
     public int AuctionId { get; set; }
 }
---
+++ src/RocketseatAuction.Domain/Enums/ECondition.cs
@@ -0,0 +1,7 @@
+﻿namespace RocketseatAuction.Domain.Enums;
+public enum ECondition
+{
+    NEW = 0,
+    GREAT = 1,
+    GOOD = 2
+}
