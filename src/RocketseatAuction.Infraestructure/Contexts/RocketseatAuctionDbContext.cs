9bdb53ed106928bfb1cbc382e4eacbc1edf75cf1
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2024-02-07 20:13:25 -0300

    Adiciona contexto para o banco de dados.

---
+++ src/RocketseatAuction.Infraestructure/Contexts/RocketseatAuctionDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.Domain.Entities;
+
+namespace RocketseatAuction.Infraestructure.Contexts;
+public class RocketseatAuctionDbContext : DbContext
+{
+    public DbSet<Auction> Auctions { get; set; }
+    public DbSet<Item> Items { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=C:\\Users\\ferna\\Documents\\Dev\\Dotnet\\Rocketseat\\leilaoDbNLW.db");
+    }
+}
